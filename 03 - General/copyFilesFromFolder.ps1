74c68231ca36b3c0436a106d4b0e6f5baf4a3751
MaKoe <m3koenig@gmail.com>
2016-01-06 13:42:03 +0100

    new Scripts!

---
+++ 03 - General/copyFilesFromFolder.ps1
@@ -0,0 +1,12 @@
+﻿$path = "C:\AAA RDP share\"
+$destination = "C:\AAA RDP share\NewFolder\"
+$filter = "*.CONFLICT"
+
+Get-ChildItem $path -Filter $filter | 
+    #%{ 
+    foreach{
+        $ConflictObject  = "$($_.FullName).txt" -replace ".CONFLICT", "";
+        Write-Host  copy file "$($ConflictObject)";
+        Copy-Item -path $ConflictObject -Destination $destination -Container -Force -Recurse -Exclude "ConflictMerged";
+    } 
+        
---
+++ 03 - General/loopThroughFiles.ps1
@@ -0,0 +1,15 @@
+﻿cls
+
+$path = "C:\AAA RDP share\"
+$filter = "*.CONFLICT"
+
+Get-ChildItem $path -Filter $filter | 
+      #%{ 
+      foreach{
+        $currentFileWithConcat = "$($_.FullName).txt" -replace ".CONFLICT", ""
+        Write-Host  $currentFileWithConcat
+       } 
+
+
+
+    
