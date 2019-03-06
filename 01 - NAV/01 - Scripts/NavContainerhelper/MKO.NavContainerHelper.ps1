11bb7072c8c55f2e0991b589893ff33c8997eca0
Matthias König <you@example.com>
2019-03-06 16:02:33 +0100

    splatting test

--- 01 - NAV/01 - Scripts/NavContainerhelper/MKO.NavContainerHelper.ps1
+++ 01 - NAV/01 - Scripts/NavContainerhelper/MKO.NavContainerHelper.ps1
@@ -5,4 +5,26 @@ Write-NavContainerHelperWelcomeText
 
 
 
-New-NavContainer -accept_eula -containerName "MKODevTest" -auth NavUserPassword -imageName "mcr.microsoft.com/businesscentral/sandbox:de-ltsc2019" -useBestContainerOS -includeCSide -shortcuts Desktop -enableSymbolLoading -includeTestToolkit -doNotExportObjectsToText -updateHosts -includeTestLibrariesOnly -clickonce -Verbose
+#New-NavContainer -accept_eula -containerName "MKODevTest" -auth NavUserPassword -imageName "mcr.microsoft.com/businesscentral/sandbox:de-ltsc2019" -useBestContainerOS -includeCSide -shortcuts Desktop -enableSymbolLoading -includeTestToolkit -doNotExportObjectsToText -updateHosts -includeTestLibrariesOnly -clickonce -Verbose
+
+#$MyParameters = @{
+#    accept_eula = $true
+#    containerName = "MKODevTest" 
+#    auth = "NavUserPassword"
+#    imageName = "mcr.microsoft.com/businesscentral/sandbox:de-ltsc2019" 
+#    useBestContainerOS = $true
+#    includeCSide = $true
+#    shortcuts = "Desktop"
+#    enableSymbolLoading = $true
+#    includeTestToolkit = $true
+#    doNotExportObjectsToText = $true
+#    updateHosts = $true
+#    includeTestLibrariesOnly = $true
+#    clickonce = $true
+#    Verbose = $true
+#}
+#
+#
+#New-NavContainer $MyParameters 
+
+New-NavContainer -accept_eula -containerName "MKODevTest" -auth NavUserPassword -imageName "mcr.microsoft.com/businesscentral/sandbox:de" -useBestContainerOS -includeCSide -shortcuts Desktop -enableSymbolLoading -includeTestToolkit -doNotExportObjectsToText -updateHosts -includeTestLibrariesOnly -clickonce -Verbose 
