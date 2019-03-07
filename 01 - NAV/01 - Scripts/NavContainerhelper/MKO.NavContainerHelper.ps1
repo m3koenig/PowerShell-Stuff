81bc4088f1441b9e586ae9e96759401e78996697
Matthias König <you@example.com>
2019-03-07 23:18:56 +0100

    ImageTag Auswahl & Ordner für Shortcuts verwenden

--- 01 - NAV/01 - Scripts/NavContainerhelper/MKO.NavContainerHelper.ps1
+++ 01 - NAV/01 - Scripts/NavContainerhelper/MKO.NavContainerHelper.ps1
@@ -1,32 +1,67 @@
-install-module navcontainerhelper -force -ErrorAction Stop
-Write-Host "okay, nav containerhelper is ready!"
-Write-NavContainerHelperWelcomeText
+#install-module navcontainerhelper -force -ErrorAction Stop
+#Write-Host "okay, nav containerhelper is ready!"
+#Write-NavContainerHelperWelcomeText
 
-#$imageName = "mcr.microsoft.com/businesscentral/sandbox"
-#$tags = (Get-NavContainerImageTags -imageName $imageName).Tags | Where-Object { $_.contains("de") -and  $_.contains("13")}
-#$tags |Format-Table
+$containerName = "MKODevTest"
+$imageName = "mcr.microsoft.com/businesscentral/sandbox"
+$languageFilter = "de"
+$platformFilter = "ltsc2019"
 
+if ([string]::IsNullOrEmpty($platformFilter) -eq $false) {
+    $tags = (Get-NavContainerImageTags -imageName $imageName).Tags | Where-Object { $_.contains($languageFilter) -and $_.contains($platformFilter)}
+}
+else {
+    $tags = (Get-NavContainerImageTags -imageName $imageName).Tags | Where-Object { $_.contains($languageFilter)}
+}
 
-#New-NavContainer -accept_eula -containerName "MKODevTest" -auth NavUserPassword -imageName "mcr.microsoft.com/businesscentral/sandbox:de-ltsc2019" -useBestContainerOS -includeCSide -shortcuts Desktop -enableSymbolLoading -includeTestToolkit -doNotExportObjectsToText -updateHosts -includeTestLibrariesOnly -clickonce -Verbose
+#mit Gui
+$choosenImageTag = $tags | Sort-Object -Descending | Out-GridView -PassThru -Title "Choose your Image of $($imageName)" 
+if ([string]::IsNullOrEmpty($choosenImageTag) -eq $true) {
+    Write-Error "No image selected..." -ErrorAction Stop
+}
+$choosenImageTag = "$($imageName):$($choosenImageTag)"
 
-#$MyParameters = @{
-#    accept_eula = $true
-#    containerName = "MKODevTest" 
-#    auth = "NavUserPassword"
-#    imageName = "mcr.microsoft.com/businesscentral/sandbox:de-ltsc2019" 
-#    useBestContainerOS = $true
-#    includeCSide = $true
-#    shortcuts = "Desktop"
-#    enableSymbolLoading = $true
-#    includeTestToolkit = $true
-#    doNotExportObjectsToText = $true
-#    updateHosts = $true
-#    includeTestLibrariesOnly = $true
-#    clickonce = $true
-#    Verbose = $true
+#Mit Console...to-do
+#$cnt = 0
+#foreach ($tag in $tags) {
+#    $cnt += 1
+#    Write-Host "[$($cnt)] - $($tag)"
+#
+#    if (($cnt % 10) -eq 0)
+#    {
+#        $NextTen = Read-Host -Prompt 'next ten?[Y]'
+#        if ($NextTen.ToUpper() -ne "Y")
+#        {
+#            break;
+#        }
+#    }
 #}
-#
-#
-#New-NavContainer $MyParameters 
+#Write-Host -ForegroundColor Green "$($tags[1])"
 
-New-NavContainer -accept_eula -containerName "MKODevTest" -auth NavUserPassword -imageName "mcr.microsoft.com/businesscentral/sandbox:de" -useBestContainerOS -includeCSide -shortcuts Desktop -enableSymbolLoading -includeTestToolkit -doNotExportObjectsToText -updateHosts -includeTestLibrariesOnly -clickonce -Verbose 
+
+New-NavContainer -accept_eula `
+    -containerName $containerName `
+    -auth NavUserPassword `
+    -imageName $choosenImageTag `
+    -useBestContainerOS `
+    -includeCSide `
+    -shortcuts Desktop `
+    -enableSymbolLoading `
+    -includeTestToolkit `
+    -doNotExportObjectsToText `
+    -updateHosts `
+    -includeTestLibrariesOnly `
+    -clickonce `
+    -Verbose 
+
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$containerShortcutFolder = Join-Path $DesktopPath $containerName
+if (!(Test-Path $containerShortcutFolder)) {
+    New-Item -ItemType directory -Path $containerShortcutFolder 
+}
+
+Get-ChildItem $DesktopPath -Filter "*$containerName*.lnk" | 
+    ForEach-Object {        
+    Move-Item -Path $_.FullName -Destination $containerShortcutFolder -Force 
+} 
